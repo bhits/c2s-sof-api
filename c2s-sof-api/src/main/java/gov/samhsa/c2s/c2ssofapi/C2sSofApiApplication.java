cf242be2fbaa66747cb83f42df77e9e530e286ff
JiahaoLi <Jiahao.Li@feisystems.com>
2018-06-13 21:44:46 -0400

    Create initial spring boot project
    #20202: As a developer, I will implement Consent2Share as SMART App, so that it can be utilized in OCP SMART on FHIR Platform

---
+++ c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/C2sSofApiApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s.c2ssofapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2sSofApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2sSofApiApplication.class, args);
+    }
+}
