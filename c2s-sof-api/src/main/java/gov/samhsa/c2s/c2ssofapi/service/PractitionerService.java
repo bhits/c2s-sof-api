371f3c18dad5965f711c7cea2dc2eee33489b074
JiahaoLi <Jiahao.Li@feisystems.com>
2018-07-19 22:57:06 -0400

    Implement to remove organization context when get all practitioners
    #20764: C2S SoF App: Issues when used with secured FHIR server and workflows when fetching consents

--- c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/PractitionerService.java
+++ c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/PractitionerService.java
@@ -9,6 +9,8 @@ import java.util.Optional;
 public interface
 PractitionerService {
     PractitionerDto getPractitioner(String practitionerId);
+
     PageDto<PractitionerDto> searchPractitioners(Optional<PractitionerController.SearchType> type, Optional<String> value, Optional<String> organization, Optional<Boolean> showInactive, Optional<Integer> page, Optional<Integer> size, Optional<Boolean> showAll);
-    PageDto<PractitionerDto> getPractitionersByOrganizationAndRole(String organization, Optional<String> role, Optional<Integer> pageNumber, Optional<Integer> pageSize);
+
+    PageDto<PractitionerDto> getPractitioners(Optional<Integer> pageNumber, Optional<Integer> pageSize);
 }
