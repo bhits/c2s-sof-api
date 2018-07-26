2066e9691e7b8782c45003b97819a1aea3fb8499
Shruti Rao <shruti.rao@feisystems.com>
2018-07-26 14:41:05 -0400

    Add StructureDefinitionEnum
    #20631:As developer, I can validate resource operation (create/update/delete) against corresponding resource profile, so that I can determine if the operation can continue or not

--- c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/util/FhirOperationUtil.java
+++ c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/util/FhirOperationUtil.java
@@ -4,19 +4,26 @@ import ca.uhn.fhir.rest.api.CacheControlDirective;
 import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.gclient.IQuery;
+import ca.uhn.fhir.rest.gclient.TokenClientParam;
 import ca.uhn.fhir.rest.server.exceptions.BaseServerResponseException;
 import ca.uhn.fhir.validation.FhirValidator;
 import ca.uhn.fhir.validation.ValidationResult;
 import gov.samhsa.c2s.c2ssofapi.config.ConfigProperties;
+import gov.samhsa.c2s.c2ssofapi.domain.StructureDefinitionEnum;
 import gov.samhsa.c2s.c2ssofapi.service.exception.FHIRClientException;
 import gov.samhsa.c2s.c2ssofapi.service.exception.FHIRFormatErrorException;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.dstu3.model.Bundle;
 import org.hl7.fhir.dstu3.model.DomainResource;
+import org.hl7.fhir.dstu3.model.ResourceType;
+import org.hl7.fhir.dstu3.model.StructureDefinition;
+import org.hl7.fhir.dstu3.model.UriType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static ca.uhn.fhir.rest.api.Constants.PARAM_LASTUPDATED;
 
@@ -110,6 +117,130 @@ public class FhirOperationUtil {
             }
         }
         return bundleEntryComponents;
+    }
+
+    public static List<UriType> getURIList(IGenericClient fhirClient, String resource) {
+        Bundle structureDefinitionBundle = null;
+
+        switch (resource.toUpperCase()) {
+            case "APPOINTMENT":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Appointment"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "CARETEAM":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("CareTeam"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "COMMUNICATION":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Communication"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "CONSENT":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Consent"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "COVERAGE":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Coverage"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "EPISODEOFCARE":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("EpisodeOfCare"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "FLAG":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Flag"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "HEALTHCARESERVICE":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("HealthcareService"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "LOCATION":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Location"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "ORGANIZATION":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Organization"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "PATIENT":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Patient"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "PRACTITIONER":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Practitioner"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "PRACTITIONERROLE":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("PractitionerRole"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "RELATEDPERSON":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("RelatedPerson"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            case "TASK":
+                structureDefinitionBundle = fhirClient.search().forResource(StructureDefinition.class)
+                        .where(new TokenClientParam("type").exactly().code("Task"))
+                        .returnBundle(Bundle.class)
+                        .execute();
+                break;
+            default:
+
+        }
+
+        if (structureDefinitionBundle != null && !structureDefinitionBundle.getEntry().isEmpty()) {
+            //First check in server
+            log.info("Number of Structure Definitions found:" + structureDefinitionBundle.getTotal() + " for " + resource);
+
+            List<StructureDefinition> structureDefinitionList = structureDefinitionBundle.getEntry().stream()
+                    .filter(bundle -> bundle.getResource().getResourceType().equals(ResourceType.StructureDefinition))
+                    .map(structureDefinition -> (StructureDefinition) structureDefinition.getResource())
+                    .collect(Collectors.toList());
+            return structureDefinitionList.stream().map(StructureDefinition::getUrlElement).collect(Collectors.toList());
+        } else {
+            //Return URI List from ENUM
+            log.info("No StructureDefinition found...Getting URL from ENUM for " + resource);
+            try {
+                String url = StructureDefinitionEnum.valueOf(resource.toUpperCase()).getUrl();
+                if (url != null && !url.isEmpty()) {
+                    return Collections.singletonList(new UriType(url));
+                }
+            } catch (Exception e) {
+                log.error("Neither StructureDefinition nor ENUM constant found");
+                // Don't get stuck here
+                return null;
+            }
+        }
+        return null;
     }
 }
 
