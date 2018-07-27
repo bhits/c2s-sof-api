44dfda896316d16cf2f2c3a87902ef2ae1d1b7a3
Shruti Rao <shruti.rao@feisystems.com>
2018-07-27 11:22:27 -0400

    Add additional profile meta data
    #20631:As developer, I can validate resource operation (create/update/delete) against corresponding resource profile, so that I can determine if the operation can continue or not

--- c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/util/FhirProfileUtil.java
+++ c2s-sof-api/src/main/java/gov/samhsa/c2s/c2ssofapi/service/util/FhirProfileUtil.java
@@ -2,8 +2,10 @@ package gov.samhsa.c2s.c2ssofapi.service.util;
 
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.dstu3.model.ActivityDefinition;
 import org.hl7.fhir.dstu3.model.Consent;
 import org.hl7.fhir.dstu3.model.Meta;
+import org.hl7.fhir.dstu3.model.Organization;
 import org.hl7.fhir.dstu3.model.ResourceType;
 import org.hl7.fhir.dstu3.model.UriType;
 
@@ -17,6 +19,22 @@ public class FhirProfileUtil {
         if (uriList != null && !uriList.isEmpty()) {
             Meta meta = new Meta().setProfile(uriList);
             consent.setMeta(meta);
+        }
+    }
+
+    public static void setOrganizationProfileMetaData(IGenericClient fhirClient, Organization organization) {
+        List<UriType> uriList = FhirOperationUtil.getURIList(fhirClient, ResourceType.Organization.toString());
+        if (uriList != null && !uriList.isEmpty()) {
+            Meta meta = new Meta().setProfile(uriList);
+            organization.setMeta(meta);
+        }
+    }
+
+    public static void setActivityDefinitionProfileMetaData(IGenericClient fhirClient, ActivityDefinition activityDefinition) {
+        List<UriType> uriList = FhirOperationUtil.getURIList(fhirClient, ResourceType.ActivityDefinition.toString());
+        if (uriList != null && !uriList.isEmpty()) {
+            Meta meta = new Meta().setProfile(uriList);
+            activityDefinition.setMeta(meta);
         }
     }
 }
